94e098e95ecc8c2daed0621b5a3a6ecb0068d78f
이세용 <79912032+pla2n@users.noreply.github.com>
2026-01-15 13:53:03 +0900

    Refactor CommentController request mappings

--- BE/aivleTK/src/main/java/com/nara/aivleTK/controller/CommentController.java
+++ BE/aivleTK/src/main/java/com/nara/aivleTK/controller/CommentController.java
@@ -11,11 +11,12 @@ import java.util.List;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/bids/{bidId}/comments")
+@RequestMapping("/api") // [수정 1] 공통 시작 주소인 "/api"만 남깁니다.
 public class CommentController {
     private final CommentService commentService;
 
-    @PostMapping("/bids/{bidId}/comments")
+    // 1. 입찰(Bid) 댓글 작성
+    @PostMapping("/bids/{bidId}/comments") // [수정 2] 여기서 구체적인 주소를 적어줍니다.
     public ResponseEntity<CommentResponse> createBidComment(
             @PathVariable("bidId") int bidId,
             @RequestBody CommentCreateRequest request
@@ -24,6 +25,7 @@ public class CommentController {
         return ResponseEntity.ok(response);
     }
 
+    // 2. 입찰(Bid) 댓글 조회
     @GetMapping("/bids/{bidId}/comments")
     public ResponseEntity<List<CommentResponse>> getCommentsByBid(
             @PathVariable("bidId") int bidId
@@ -32,7 +34,8 @@ public class CommentController {
         return ResponseEntity.ok(comments);
     }
 
-    @PostMapping("/boards/{boardId}/comments")
+    // 3. 게시글(Board) 댓글 작성
+    @PostMapping("/boards/{boardId}/comments") // [수정 3] 이제 Board 주소도 자유롭게 쓸 수 있습니다.
     public ResponseEntity<CommentResponse> createBoardComment(
             @PathVariable("boardId") int boardId,
             @RequestBody CommentCreateRequest request
@@ -41,21 +44,22 @@ public class CommentController {
         return ResponseEntity.ok(response);
     }
 
+    // 4. 게시글(Board) 댓글 조회
     @GetMapping("/boards/{boardId}/comments")
     public ResponseEntity<List<CommentResponse>> getCommentsByBoard(
             @PathVariable("boardId") int boardId
     ){
-        // 서비스에 Board용 조회 메서드 필요 (ex: findAllByBoard_Id)
         List<CommentResponse> comments = commentService.getCommentsByBoard(boardId);
         return ResponseEntity.ok(comments);
     }
 
-    @DeleteMapping("/{commentId}")
+    // 5. 댓글 삭제 (공통)
+    @DeleteMapping("/comments/{commentId}") // [수정 4] 삭제는 보통 부모 ID 없이 댓글 ID로만 합니다.
     public ResponseEntity<String> deleteComment(
             @PathVariable int commentId,
             @RequestParam("userId") int userId
     ){
-        commentService.deleteComment(commentId,userId);
+        commentService.deleteComment(commentId, userId);
         return ResponseEntity.ok("댓글이 삭제되었습니다.");
     }
 }
